b661efb56dafb4796fd01d371b7914619d393905
Alexsander Leal <alexsander.leal86@gmail.com>
2024-11-24 13:12:44 +0000

    Configuração de ambiente
    Foi criado o projeto no Spring Initializr, foi configurado e baixada as depêndencias iniciais para o funcionamento do projeto. Também foram baixada extensões que ajudarão no desenvolvimento no VS Code.

---
+++ src/main/java/com/ticket_tool/easyhelp/EasyhelpApplication.java
@@ -0,0 +1,13 @@
+package com.ticket_tool.easyhelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyhelpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyhelpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ticket_tool/easyhelp/EasyhelpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticket_tool.easyhelp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyhelpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
